f7fc833ccb2b2261412d19718a793acd459616ca
USER <USER@10.52.33.237>
2019-02-01 14:07:07 +0300

    LabTwoTasks1

---
+++ src/labtwotasks1/LabTwoTasks1.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package labtwotasks1;
+
+/**
+ *
+ * @author USER
+ */
+public class LabTwoTasks1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a=25;
+        int b=5;
+        int result=a*b;
+        
+        System.out.println("25*5="+result);
+    }
+    
+}
